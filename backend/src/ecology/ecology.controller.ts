6e67dca112d6d73ba3bb42871ae33d4a8569dc17
João Vitor <thecryzin@gmail.com>
2025-04-12 12:05:53 -0300

    fix: remove comments

--- backend/src/ecology/ecology.controller.ts
+++ backend/src/ecology/ecology.controller.ts
@@ -8,11 +8,11 @@ export class EcologyController {
   @Get()
   getSensorData(@Query('data') data: string) {
     const start = new Date(data)
-    start.setHours(0, 0, 0, 0) // Garante que o horário é 00:00:00
+    start.setHours(0, 0, 0, 0)
 
     const end = new Date(start)
-    end.setDate(end.getDate() + 1) // Dia seguinte
-    end.setHours(0, 0, 0, 0) // Garante que também é 00:00:00
+    end.setDate(end.getDate() + 1)
+    end.setHours(0, 0, 0, 0)
 
     return this.ecologyService.findBetween(start, end)
   }
